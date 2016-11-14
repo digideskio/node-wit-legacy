b20dd6778bb5b1deae55005dc928574eef5c42d5
Eetu Tuomala <eetu.tuomala@sc5.io>
2016-11-14 12:22:55 +0200

    adds test for node-wit functions and 6.9.0 node to travis

--- tests/lib.js
+++ tests/lib.js
@@ -1,5 +1,17 @@
 'use strict';
 
+const wit = require('node-wit');
+const expect = require('chai').expect;
+
+describe('node-wit', () => {
+  it('should have functions log, Wit and interactive', (done) => {
+    const witFunctions = Object.keys(wit);
+    expect(witFunctions).to.be.length(3);
+    expect(witFunctions).to.eql(['log', 'Wit', 'interactive']);
+    done();
+  });
+});
+
 describe('lib', () => {
   require('./common').runTests(require('../lib'));
 });

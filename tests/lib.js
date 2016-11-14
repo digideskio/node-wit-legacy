2482a9e866249bcf5a9c2a37d5b465f8b0ea9f6d
Eetu Tuomala <eetu.tuomala@sc5.io>
2016-11-14 12:45:18 +0200

    adds Wit and interactive existence checks

--- tests/lib.js
+++ tests/lib.js
@@ -6,7 +6,6 @@ const expect = require('chai').expect;
 describe('node-wit', () => {
   it('should have functions log, Wit and interactive', (done) => {
     const witFunctions = Object.keys(wit);
-    expect(witFunctions).to.be.length(3);
     expect(witFunctions).to.eql(['log', 'Wit', 'interactive']);
     done();
   });
